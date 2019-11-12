55beeec88a582b5b14e8492e66a7c5c616e22888
Filipe Junqueira <filipelqj@gmail.com>
2019-11-12 17:21:11 -0300

    HellO

--- Df_averaging.py
+++ Df_averaging.py
@@ -2,7 +2,8 @@ import numpy as np
 from ImportMatrixFile import import_matrix_file
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib.transforms as trfm
 
 root = tk.Tk()
 root.withdraw()  # prevents the default window  to open.
@@ -10,14 +11,34 @@ file_path = (
     askopenfilename()
 )  # show an "Open" dialog box and return the path to the selected file
 
-image, image_raw = import_matrix_file(1, file_path)
+
+print("Type 0: up forward, 1: up retrace, 2: down forward, 3: down retrace")
+file_index = int(input())
+image, image_raw = import_matrix_file(file_index, file_path)
 
 # Averaging image per row
 mean_array = np.mean(image, axis=1, dtype=np.float64)
 
+# Getting image size (number of points) and image height
 
-fig = plt.figure()
+image_height = image_raw.height
+image_size = image.shape[0]
+
+# Creating an axis to plot the average value of Df
+x_axis = np.arange(start=0.0, stop=image_height, step=image_height / image_size)
+
 temp_title = file_path.split("--")
-plot_title = temp_title[1].replace(".txt", "")
-plt.plot(mean_array)
+plot_title = temp_title[1].replace("_mtrx", "")
+
+plt.plot(x_axis / 10 ** (-9), mean_array, "#6699CC")
+plt.ylabel("Frequency shift | df(Z)[Hz]")
+plt.title(plot_title + " Line average")
+plt.xlabel("Z[nm]")
+
+plt.savefig(
+    "D:/LTData/2019-07-11/traces/" + plot_title + "_Line_average.png",
+    bbox_inches="tight",
+    transparent=True,
+)
+
 plt.show()
--- GwideonTracesImport.py
+++ GwideonTracesImport.py
@@ -1,6 +1,7 @@
 # Script for opening .txt and saving traces exported from Gwideon.
 import numpy as np
 import matplotlib.pyplot as plt
+from subprocess import check_output
 
 # First step is to locate file. One method is to manually select the spectroscopy txt .file.
 # To do this one must use the following code:
@@ -13,6 +14,13 @@ file_path = (
     askopenfilename()
 )  # show an "Open" dialog box and return the path to the selected file
 
-data = np.loadtxt(file_path)
+trace = np.loadtxt(file_path, skiprows=3)
 
+def get_one_line(filepath, line_number):
+    return check_output(["sed", "-n", "%sp" % line_number, filepath])
 
+units = get_one_line(file_path, 3).decode("utf-8").strip().split("    ")
+
+plt.plot(trace[:,0],trace[:,1])
+
+plt.show()
