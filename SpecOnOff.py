276fe459d6bc1495c0e13989624e7cc6366fbe7f
Filipe Junqueira <filipelqj@gmail.com>
2019-11-24 13:17:45 -0300

    TODO + Graphs details

--- SpecOnOff.py
+++ SpecOnOff.py
@@ -18,6 +18,16 @@ data_retrace_diff = np.asarray(
     [x - y for x, y in zip(data_on_tuple[1][:, 1], data_off_tuple[1][:, 1])]
 )
 
-plt.plot(data_on_tuple[0][:, 0], data_forward_diff, "#163A37")
-plt.plot(data_on_tuple[1][:, 0], data_retrace_diff, "#E3B23C")
+#TODO add the tick positions in this graph as I did in loadspecsript.py.
+
+plot_title_on = data_on_path.split("--")[1].replace(".txt", "")
+plot_title_off = data_off_path.split("--")[1].replace(".txt", "")
+plot_title = f"Df difference = {plot_title_on} (ON) - {plot_title_off} (OFF)"
+plt.plot(data_on_tuple[0][:, 0], data_forward_diff, "#2A9D8F")
+plt.plot(data_on_tuple[1][:, 0], data_retrace_diff, "#E9C46A")
+plt.title(plot_title)
+plt.xlabel("Z[nm]")
+plt.ylabel("Df[Hz]")
+
+plt.savefig("D:/LTData/2019-07-11/specs/" + plot_title + ".png",bbox_inches="tight",)
 plt.show()
