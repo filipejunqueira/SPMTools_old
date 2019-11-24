5b6025987ae8332395b375dd3dbc7da6d8e6c5ac
Filipe Junqueira <filipelqj@gmail.com>
2019-11-24 13:16:47 -0300

    This is only a test file.

--- testscript.py
+++ testscript.py
@@ -1,6 +1,9 @@
 # I only do tests here
+import numpy as np
+import matplotlib.pyplot as plt
+from spmFunctions import get_one_line, get_path_gui
 
-a = [1, 2, 3, 4, 5, 6]
-b = [2, 2, 3, 4, 5, 7]
+file_path = get_path_gui()
+data = np.genfromtxt(file_path,skip_header=3)
 
-print([x - y for x, y in zip(a, b)])
+
