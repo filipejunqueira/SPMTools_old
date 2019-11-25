1d36eaffac4782c9c4f7c8854d69cd01d92b106d
Filipe Junqueira <filipelqj@gmail.com>
2019-11-25 15:20:04 -0300

    Added new function to plot specs.

--- spmFunctions.py
+++ spmFunctions.py
@@ -3,7 +3,9 @@ import numpy as np
 from subprocess import check_output
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
+import matplotlib.pyplot as plt
 from pathlib import Path
+
 # Function that imports matrix file
 def import_matrix_file(series_number, file_path):
 
@@ -13,7 +15,7 @@ def import_matrix_file(series_number, file_path):
     image = im.data
 
     # dict = {0: "up forward", 1: "up retrace", 2: "down forward", 3: "down retrace"}
-    #print("Image has been uploaded to variables image and im")
+    # print("Image has been uploaded to variables image and im")
 
     return image, im
 
@@ -55,8 +57,8 @@ def load_spec(file_path):
             self.size = size
             self.position = position
 
-    spec_object = Spec(data_forward,data_retrace,data_size,spec_position)
-    return  data_forward, data_retrace, data_size, spec_position, data, spec_object
+    spec_object = Spec(data_forward, data_retrace, data_size, spec_position)
+    return data_forward, data_retrace, data_size, spec_position, data, spec_object
 
 
 # grabs one specific line of a .txt file
@@ -69,8 +71,61 @@ def get_one_line(filepath, line_number):
 def get_path_gui():
     root = tk.Tk()
     root.withdraw()  # prevents the default window  to open.
-    root.attributes("-topmost", True) #makes the window go on top of other applications
+    root.attributes(
+        "-topmost", True
+    )  # makes the window go on top of other applications
     file_path = (
         askopenfilename()
     )  # show an "Open" dialog box and return the path to the selected file, function path makes / -> \ for windows
     return file_path
+
+
+def plot_spec(file_path):
+    spec = load_spec(file_path)[5]
+    root_path = file_path.split("/specs/")[0]
+    temp_title = file_path.split("--")
+    plot_title = temp_title[1].replace(".txt", "")
+    fig = plt.figure()
+    plt.plot(spec.forward[:, 0] / 10 ** (-9), spec.forward[:, 1], "#06D6A0")
+    plt.plot(spec.retrace[:, 0] / 10 ** (-9), spec.retrace[:, 1], "#FFD166")
+    plt.ylabel("Frequency shift | df(Z)[Hz]")
+    plt.title(plot_title)
+    plt.xlabel("Z[nm]")
+
+    # setting up x ticks and y ticks - This is important so the last and first ticks are visible
+    xticks_array = np.around(
+        np.linspace(
+            min(spec.forward[:, 0] / 10 ** (-9)),
+            max(spec.forward[:, 0] / 10 ** (-9)),
+            num=5,
+        ),
+        2,
+    )
+
+    y_max = max(np.append(spec.forward[:, 1], spec.retrace[:, 1]))
+    y_min = min(np.append(spec.forward[:, 1], spec.retrace[:, 1]))
+
+    yticks_array = np.around(np.linspace(y_min, y_max, num=5), 3)
+    plt.xticks(xticks_array)
+    plt.yticks(yticks_array)
+
+    # Text showing the positions of the spec. It just copy that from the positions from the .txt
+    # It puts this text in the lower right coner of the the figure.
+
+    textposx = (
+        min(spec.forward[:, 0] / 10 ** (-9)) + max(spec.forward[:, 0] / 10 ** (-9))
+    ) / 2
+    textposy = (y_max + y_min) / 2
+
+    plt.text(
+        textposx * (1.1),
+        textposy * (1.5),
+        f"Spec pos: {spec.position}",
+        fontsize=7,
+        family="serif",
+    )
+
+    plt.savefig(
+        f"{root_path}/specs/{plot_title}.png", bbox_inches="tight", transparent=True,
+    )
+    return True
